5591e2d36b90a8bd7c28925850a655c7a4a367f4
antoniobarbalau <antoniobarbalau@gmail.com>
2018-01-04 13:01:27 +0200

    v6.0

--- step2_i3_config.sh
+++ step2_i3_config.sh
@@ -3,9 +3,14 @@ conda install -c auto pyconfig
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ets-labs/python-vimrc/master/setup.sh)"
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
 
+sudo apt install feh
 
 mkdir ~/.config/mpd
 cp ./mpd.conf ~/.config/mpd/mpd.conf
 cp ./i3_config ~/.config/i3/config
 sudo cp ./i3status.conf /etc/i3status.conf
+cp wall.jpg ~/Pictures/
+
+echo "set -o vi" >> ~/.bashrc
+echo "set number relativenumber" >> ~/.vimrc
 
