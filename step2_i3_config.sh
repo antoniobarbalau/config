ef43d18ff7f10248e86df4c56ef0a01027c4ba60
antoniobarbalau <antoniobarbalau@gmail.com>
2018-02-01 22:29:39 +0200

    v6.6.6

--- step2_i3_config.sh
+++ step2_i3_config.sh
@@ -1,5 +1,6 @@
 conda install -c auto pyconfig
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/
 vi +PluginInstall
 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
