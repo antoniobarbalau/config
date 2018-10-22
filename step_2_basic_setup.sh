96ec96349bbc1e94567daf260f5e91e5f0f5e2b3
antoniobarbalau <antoniobarbalau@gmail.com>
2018-10-23 01:14:30 +0300

    i3lock

--- step_2_basic_setup.sh
+++ step_2_basic_setup.sh
@@ -29,6 +29,7 @@ sudo apt install -y feh
 mkdir ~/.config/mpd
 cp ./mpd.conf ~/.config/mpd/mpd.conf
 cp ./i3_config ~/.config/i3/config
+cp ./lock.sh ~/.config/i3/
 sudo cp ./i3status.conf /etc/i3status.conf
 cp wallpaper.jpg ~/Pictures/
 
