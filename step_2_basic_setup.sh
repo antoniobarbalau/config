421e06c00b236c72abf46090518e60b159ba2e13
antoniobarbalau <antoniobarbalau@gmail.com>
2018-11-28 20:55:06 +0200

    zathura

--- step_2_basic_setup.sh
+++ step_2_basic_setup.sh
@@ -39,6 +39,8 @@ sudo apt install -y ranger
 ranger --copy-config=all
 cp ./rc.conf ~/.config/ranger/
 
+sudo apt install zathura zathura-cb zathura-dev zathura-djvu zathura-pdf-poppler zathura-ps
+
 sudo sed -i "s/quiet splash/quiet splash acpi_backlight=vendor acpi_osi='!Windows 2013' acpi_osi='!Windows 2012'/" /etc/default/grub
 sudo update-grub
 
