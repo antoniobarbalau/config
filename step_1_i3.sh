93fb1ede30eb22e993278f875a0ad8a011aa6c5c
antoniobarbalau <antoniobarbalau@gmail.com>
2020-03-25 16:16:09 +0200

    refactored

--- step_1_i3.sh
+++ step_1_i3.sh
@@ -1,20 +1,3 @@
-apt install -y libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev \
-libxcb-util0-dev libxcb-icccm4-dev libyajl-dev \
-libstartup-notification0-dev libxcb-randr0-dev \
-libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
-libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
-autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
-
-git clone https://www.github.com/Airblader/i3 i3-gaps
-cd i3-gaps
-
-autoreconf --force --install
-rm -rf build/
-mkdir -p build && cd build/
-
-../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-make
-sudo make install
-
-sudo apt install -y dmenu i3lock i3status dunst
-
+sudo add-apt-repository ppa:kgilmer/speed-ricer
+sudo apt update
+sudo apt install i3-gaps
--- step_2_basic_setup.sh
+++ step_2_basic_setup.sh
@@ -2,37 +2,22 @@ git config --global user.name "antoniobarbalau"
 git config --global user.email "antoniobarbalau@gmail.com"
 
 sudo apt install -y gcc g++ cmake compton neovim
-sudo apt install -y mpd ncmpcpp mpc mpv
 
 sudo apt install -y zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 cp ./.zshrc ~/
 cp ./dracula.zsh-theme ~/.oh-my-zsh/themes/
 
-wget https://repo.anaconda.com/archive/Anaconda3-2018.12-Linux-x86_64.sh
-sudo sh Anaconda3-2018.12-Linux-x86_64.sh
-sudo chown -R $USER ~/anaconda3/
-
 sudo apt install -y exuberant-ctags
-
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
 cp init.vim ~/.config/nvim/
 nvim +PluginInstall +qa
 
 sudo apt install -y feh scrot imagemagick w3m-img
 
-mkdir ~/.config/mpd
-cp ./mpd.conf ~/.config/mpd/mpd.conf
-sudo cp ./mpd.conf /etc/mpd.conf
-cp ./ncmpcpp_config ~/.ncmpcpp/config
 cp ./i3_config ~/.config/i3/config
 cp ./lock.sh ~/.config/i3/
 sudo cp ./i3status.conf /etc/i3status.conf
-cp ./pictures/absolute.jpg ~/Pictures/
-cp ./pictures/diablo.png ~/Pictures/
-cp ./pictures/green.jpeg ~/Pictures/
-cp ./pictures/fenrir.jpg ~/Pictures/
-cp ./pictures/gears_cropped.png ~/Pictures/
 cp ./pictures/rick* ~/Pictures/
 cp ./pictures/yellow\ planet.jpg ~/Pictures/
 
@@ -44,14 +29,7 @@ sudo apt install -y ranger
 ranger --copy-config=all
 cp ./rc.conf ~/.config/ranger/
 
-sudo apt install -y zathura zathura-cb zathura-dev zathura-djvu zathura-pdf-poppler zathura-ps
 sudo apt install -y texlive-full
 sudo apt install -y pavucontrol blueman
 
-sudo sed -i "s/quiet splash/quiet splash acpi_backlight=vendor acpi_osi='!Windows 2013' acpi_osi='!Windows 2012'/" /etc/default/grub
-sudo update-grub
-
-rm -r ./Anaconda3-2018.12-Linux-x86_64.sh
-
-sudo cp ./40-libinput.conf /usr/share/X11/xorg.conf.d/
 
