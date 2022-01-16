e3dc73ea1eff6882e5197d97016ccf72708b698a
Antonio Barbalau <antoniobarbalau@gmail.com>
2022-01-16 23:59:26 +0200

    Update arch_config.sh

--- arch_config.sh
+++ arch_config.sh
@@ -1,17 +1,17 @@
-sudo pacman -Syyu compton scrot imagemagick w3m-img
+sudo pacman -Sy compton scrot imagemagick w3m-img --noconfirm
 
 cp ./i3_config ~/.config/i3/config
 cp ./lock.sh ~/.config/i3/
 sudo cp ./i3status.conf /etc/i3status.conf
 
-sudo pacman -Syyu rxvt-unicode
+sudo pacman -Sy rxvt-unicode --noconfirm
 cp ./.Xresources ~/
 xrdb ~/.Xresources
 
-sudo pacman -Syyu ranger
+sudo pacman -Sy ranger --noconfirm
 ranger --copy-config=all
 cp ./rc.conf ~/.config/ranger/
 
-sudo pacman -Syyu pavucontrol blueman
+sudo pacman -Sy pavucontrol blueman --noconfirm
 
 
