1dafde9cfe9bd26e0aa86d0c5902a014ed669fe5
Antonio Barbalau <antoniobarbalau@gmail.com>
2022-01-17 01:05:34 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,5 @@
 sudo pacman -Sy compton scrot --noconfirm
 cp ./i3_config ~/.config/i3/config
-cp ./lock.sh ~/.config/i3/
 sudo cp ./i3status.conf /etc/i3status.conf
 
 sudo pacman -Sy rxvt-unicode --noconfirm
@@ -11,4 +10,6 @@ sudo pacman -Sy neovim nodejs npm --noconfirm
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim +PlugInstall +qa
 
-sudo pacman -Sy pavucontrol blueman firefox nautilus nmtui --noconfirm
+sudo pacman -Sy pavucontrol blueman firefox nautilus networkmanager --noconfirm
+sudo systemctl start NetworkManager
+sudo systemctl enable NetworkManager
